2e88f081a82385df8df7bfd44f8f2df15b971fdf
sbx_user1051 <sbx_user1051@169.254.172.69>
2022-04-09 15:32:20 +0000

    Sat Apr 09 15:32:20 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,9 +4,28 @@
 # skus = unicode string
 
 prices = {"A": 50, "B": 30, "C": 20, "D": 15}
+offers = {"3A": 130, "2B": 45}
 
 def checkout(skus):
     '''
-    skus: probably a list eg. ["A", "A", "B"]
+    skus: a string eg. "AAB"
     '''
+    # First check that all the skus are in the prices dict
+    for char in skus:
+        if char not in prices.keys():
+            return -1
+    
+    # Now add up all items to find the total
+    number_of_items = {}
+    for char in skus:
+        if char in number_of_items:
+            number_of_items[char] += 1
+        else:
+            number_of_items[char] = 1
+    
+    
+
+
+
+
 
