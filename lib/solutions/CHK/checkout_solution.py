859c13ada53638ae366362f6ef4d34f1d2153d49
sbx_user1051 <sbx_user1051@169.254.172.69>
2022-04-09 15:29:20 +0000

    Sat Apr 09 15:29:20 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,11 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+prices = {"A": 50, "B": 30, "C": 20, "D": 15}
+
 def checkout(skus):
-    raise NotImplementedError()
+    '''
+    skus: probably a list eg. ["A", "A", "B"]
+    '''
+
