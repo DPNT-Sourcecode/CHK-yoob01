0c385f0f5fb3647ac4f38fd225f80fde784d1608
sbx_user1051 <sbx_user1051@169.254.172.69>
2022-04-09 15:38:20 +0000

    Sat Apr 09 15:38:20 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -23,10 +23,17 @@ def checkout(skus):
         else:
             number_of_items[char] = 1
     
+    total_for_each_sku = {}
+    for i in number_of_items:
+        total_for_each_sku[i] = prices[i] * number_of_items[i]
+
     # Once we've added up all the different skus, check for special offers
     for sku in number_of_items:
         if sku in offers:
-            # if an sku is in an offer then we'll need to 
+            # if an sku is in an offer then we'll need to find out how many
+            # of those offers can be applied... I think? so buying 6A's should be 260
+            offer_amount = offers[sku]
+
 
 
 
