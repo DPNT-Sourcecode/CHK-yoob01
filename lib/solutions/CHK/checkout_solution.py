1d03f9be861dd2c83fdeef1dcba93835c21801eb
sbx_user1051 <sbx_user1051@169.254.122.93>
2022-04-10 11:24:31 +0000

    Sun Apr 10 11:24:31 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -34,7 +34,8 @@ def find_best_deal(prices, quantities, number_of_items, sku):
         # save that leading term
         counters[value] = leading_term
     # We return a dictionary of counters, that maps the deals to the number of those deals we can form
-    # So for example, given an 14 A's we can form 
+    # So for example, given an 14 A's we can form 2 "5 deals", 1 "3 deal" and 1 "1 deal"
+    # so the output would be {5: 2, 3: 1, 1: 1}
     return counters
 
 
@@ -56,6 +57,11 @@ def checkout(skus):
             number_of_items[char] = 1
     
     total_for_each_sku = {}
+    for sku in number_of_items:
+        deal_groupings = find_best_deal(prices, quantities, number_of_items, sku)
+        
+
+    
     for i in number_of_items:
         total_for_each_sku[i] = prices[i] * number_of_items[i]
 
@@ -93,6 +99,7 @@ def checkout(skus):
         running_total += total_for_each_sku[sku]
     
     return running_total
+
 
 
 
