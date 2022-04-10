609497198afea211fccf0e4e5c5c216fb0acb3a4
sbx_user1051 <sbx_user1051@169.254.23.53>
2022-04-10 17:59:16 +0000

    Sun Apr 10 17:59:16 UTC 2022

---
+++ tests/test_remove_sku.py
@@ -0,0 +1,5 @@
+from lib.solutions.CHK.checkout_solution import checkout, find_best_deal, remove_sku, buy_get_free
+
+def test_remove_sku_affee():
+    output = remove_sku("AFFEE", buy_get_free)
+    assert output == "AFEE"
