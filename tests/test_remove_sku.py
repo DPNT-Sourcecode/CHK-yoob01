52a62af8823488ea6dcdc45e07d2cd3a28a51791
sbx_user1051 <sbx_user1051@169.254.23.53>
2022-04-10 18:03:28 +0000

    Sun Apr 10 18:03:28 UTC 2022

--- tests/test_remove_sku.py
+++ tests/test_remove_sku.py
@@ -3,3 +3,7 @@ from lib.solutions.CHK.checkout_solution import checkout, find_best_deal, remove
 def test_remove_sku_affee():
     output = remove_sku("AFFEE", buy_get_free)
     assert output == "AFEE"
+
+def test_remove_sku_affeeffb():
+    output = remove_sku("AFFEEFFB", buy_get_free)
+    assert output == "AEEFF"
